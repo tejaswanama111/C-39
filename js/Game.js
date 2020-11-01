a82ae90f92e9d09333d07c3e670a9c4fb94de2f9
tejaswanama111 <tejaswanama2006@gmail.com>
2020-11-01 12:46:41 -0800

    end game

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1", car1Img);
     car2 = createSprite(300,200);
+    car2.addImage("car2", car2Img);
     car3 = createSprite(500,200);
+    car3.addImage("car3", car3Img);
     car4 = createSprite(700,200);
+    car4.addImage("car4", car4Img);
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +47,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background(rgb(198,135,103));
+      image(trackImg, 0, -displayHeight*4, displayWidth, displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 180;
       var y;
 
       for(var plr in allPlayers){
@@ -59,6 +64,7 @@ class Game {
         x = x + 200;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
+
         cars[index-1].x = x;
         cars[index-1].y = y;
 
@@ -67,7 +73,7 @@ class Game {
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
         }
-       
+    
         //textSize(15);
         //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
       }
@@ -79,6 +85,14 @@ class Game {
       player.update();
     }
 
+    if(player.distance > 3750){
+      gameState = 2;
+    }
+    
     drawSprites();
+  }
+
+  end(){
+    console.log("Game Over");
   }
 }
--- sketch.js
+++ sketch.js
@@ -9,7 +9,16 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
+var trackImg, car1Img, car2Img, car3Img, car4Img;
 
+function preload(){
+  trackImg = loadImage("images/track.jpg");
+  car1Img = loadImage("images/car1.png");
+  car2Img = loadImage("images/car2.png");
+  car3Img = loadImage("images/car3.png");
+  car4Img = loadImage("images/car4.png");
+
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +36,8 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState === 2){
+    game.end();
   }
 }
